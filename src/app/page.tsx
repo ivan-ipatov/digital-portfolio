8da388eb0963a51ee1defae42c3440b654c9926b
integral-ivan <ivanipatov12345@gmail.com>
2024-03-04 01:24:42 +0500

    few enhancement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import block from 'bem-cn-lite';
 import './Home.scss';
-const b = block('wrapper');
+
 export default function Home() {
     return (
-        <main className={b()}>
+        <main className="main_section">
             <h3>Site under development</h3>
             <p>by My favorite team</p>
         </main>
