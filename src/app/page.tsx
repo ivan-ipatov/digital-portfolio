73d0bd335d4f105e8363f733c0a01b2c1c4a1d7a
integral-ivan <ivanipatov12345@gmail.com>
2024-03-12 02:58:39 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import {Button} from '@gravity-ui/uikit';
 import styles from './Home.module.scss';
 import {signIn, signOut, useSession} from 'next-auth/react';
-import Image from 'next/image';
 
 export default function Home() {
     const {data: session} = useSession();
@@ -11,12 +10,12 @@ export default function Home() {
             <main className={styles['homepage-content']}>
                 <h3>Site under development</h3>
                 <p>by My favorite team</p>
-                <Image
+                <img
                     src={
                         session.user?.image ??
                         'https://vsegda-pomnim.com/uploads/posts/2022-04/thumbs/1649232741_8-vsegda-pomnim-com-p-pustoe-litso-foto-10.jpg'
                     }
-                    width={100}
+                    width={50}
                     className={styles['homepage-content__user-image']}
                     alt="Фото пользователя"
                 />
