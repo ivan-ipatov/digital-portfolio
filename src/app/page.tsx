be888aad45649a949960124a4de0b8b232600271
integral-ivan <ivanipatov12345@gmail.com>
2024-03-12 03:05:11 +0500

    center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,35 @@
 import {Button} from '@gravity-ui/uikit';
 import styles from './Home.module.scss';
 import {signIn, signOut, useSession} from 'next-auth/react';
+import block from 'bem-cn-lite';
+
+const b = block('homepage-content');
 
 export default function Home() {
     const {data: session} = useSession();
     if (session) {
         return (
-            <main className={styles['homepage-content']}>
+            <main className={styles[b()]}>
                 <h3>Site under development</h3>
                 <p>by My favorite team</p>
-                <img
-                    src={
-                        session.user?.image ??
-                        'https://vsegda-pomnim.com/uploads/posts/2022-04/thumbs/1649232741_8-vsegda-pomnim-com-p-pustoe-litso-foto-10.jpg'
-                    }
-                    width={50}
-                    className={styles['homepage-content__user-image']}
-                    alt="Фото пользователя"
-                />
+                <div>
+                    <img
+                        src={
+                            session.user?.image ??
+                            'https://vsegda-pomnim.com/uploads/posts/2022-04/thumbs/1649232741_8-vsegda-pomnim-com-p-pustoe-litso-foto-10.jpg'
+                        }
+                        width={50}
+                        className={styles[b('user-image')]}
+                        alt="Фото пользователя"
+                    />
+                </div>
                 <p>Привет, {session.user?.name}</p>
                 <Button onClick={() => signOut()}>Выйти</Button>
             </main>
         );
     } else {
         return (
-            <main className={styles['homepage-content']}>
+            <main className={styles[b()]}>
                 <h3>Site under development</h3>
                 <p>by My favorite team</p>
                 <Button onClick={() => signIn()}>Войти</Button>
