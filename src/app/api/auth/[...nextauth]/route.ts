94880ee940c67ead13353e7064a874fdc0431e0a
integral-ivan <ivanipatov12345@gmail.com>
2024-03-12 13:09:25 +0500

    maybe fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,15 @@ const handler = NextAuth({
         VkProvider({
             clientId: process.env.VK_CLIENT_ID ?? '',
             clientSecret: process.env.VK_CLIENT_SECRET ?? '',
+            profile(result) {
+                const profile = result.response?.[0] ?? {};
+                return {
+                    id: profile.id,
+                    name: [profile.first_name, profile.last_name].filter(Boolean).join(' '),
+                    email: profile.email,
+                    image: profile.photo_100,
+                };
+            },
         }),
         // ...add more providers here
     ],
