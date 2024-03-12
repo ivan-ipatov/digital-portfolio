ec3d714854848d5becb3c2360af98c7eab6037eb
integral-ivan <ivanipatov12345@gmail.com>
2024-03-12 12:28:07 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,45 @@ const b = block('homepage-content');
 
 export default function Home() {
     const {data: session} = useSession();
-    if (session) {
-        return (
-            <main className={styles[b()]}>
-                <h3>Site under development</h3>
-                <p>by My favorite team</p>
-                <Suspense fallback={<Skeleton className={styles[b('skeleton')]} />}>
-                    <UserAvatar />
-                    <p>Привет, {<GetUserName />}</p>
-                </Suspense>
+    return (
+        <main className={styles[b()]}>
+            <h3>Site under development</h3>
+            <p>by My favorite team</p>
+            <Suspense fallback={<Skeleton className={styles[b('skeleton')]} />}>
+                {session ? (
+                    [
+                        <div key={1}>
+                            <UserAvatar />
+                            <p>Привет, {<GetUserName />}</p>
+                            <Button onClick={() => signOut()}>Выйти</Button>
+                        </div>,
+                    ]
+                ) : (
+                    <Button onClick={() => signIn()}>Войти</Button>
+                )}
+            </Suspense>
+        </main>
+    );
+    // if (session) {
+    //     return (
+    //         <main className={styles[b()]}>
+    //             <h3>Site under development</h3>
+    //             <p>by My favorite team</p>
+    //             <Suspense fallback={<Skeleton className={styles[b('skeleton')]} />}>
+    //                 <UserAvatar />
+    //                 <p>Привет, {<GetUserName />}</p>
+    //             </Suspense>
 
-                <Button onClick={() => signOut()}>Выйти</Button>
-            </main>
-        );
-    } else {
-        return (
-            <main className={styles[b()]}>
-                <h3>Site under development</h3>
-                <p>by My favorite team</p>
-                <Button onClick={() => signIn()}>Войти</Button>
-            </main>
-        );
-    }
+    //             <Button onClick={() => signOut()}>Выйти</Button>
+    //         </main>
+    //     );
+    // } else {
+    //     return (
+    //         <main className={styles[b()]}>
+    //             <h3>Site under development</h3>
+    //             <p>by My favorite team</p>
+    //             <Button onClick={() => signIn()}>Войти</Button>
+    //         </main>
+    //     );
+    // }
 }
