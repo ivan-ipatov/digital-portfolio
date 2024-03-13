7a68a72c816feee8ea4eeca07a76d73e58899c34
integral-ivan <ivanipatov12345@gmail.com>
2024-03-13 14:44:04 +0500

    new

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,15 +6,13 @@ const handler = NextAuth({
         VkProvider({
             clientId: process.env.VK_CLIENT_ID as string,
             clientSecret: process.env.VK_CLIENT_SECRET as string,
-            authorization: {params: {scope: 'phone_number'}},
-            userinfo: {params: {fields: 'email'}},
-            profile(result) {
-                const profile = result.response?.[0] ?? {};
+            authorization: {params: {scope: '0'}},
+            profile(profile) {
                 return {
                     id: profile.id,
                     name: [profile.first_name, profile.last_name].filter(Boolean).join(' '),
                     email: profile.email,
-                    image: profile.photo_100,
+                    image: profile.photo_200,
                 };
             },
         }),
