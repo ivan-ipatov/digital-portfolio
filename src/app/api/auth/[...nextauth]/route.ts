98b83f10d3030b0def21240908d4b96fb9fa9422
integral-ivan <ivanipatov12345@gmail.com>
2024-03-13 15:05:41 +0500

    123

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,9 +11,9 @@ const handler = NextAuth({
                 const profile = result.response?.[0] ?? {};
                 return {
                     id: profile.id,
-                    name: [profile.first_name, profile.last_name].filter(Boolean).join(' '),
+                    name: [profile.last_name].filter(Boolean).join(' '),
                     email: profile.email,
-                    image: profile.photo_200,
+                    image: profile.photo_100,
                 };
             },
             allowDangerousEmailAccountLinking: true,
